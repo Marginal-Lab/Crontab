b99bb36e0d22480c4bf24954db5dbc3bc9de55a5
MediocreMario <maming0292@gmail.com>
2024-04-24 20:15:28 +0800

    local_etf_log

--- Crontab/hft.py
+++ Crontab/hft.py
@@ -7,6 +7,8 @@ from .base import crontab_dir
 
 def append_jobs(jobs):
     jobs.append(gen_hft_1d())
+    jobs.append(local_etf_log())
+    
 
 
 class gen_hft_1d(AbstractJob):
@@ -25,5 +27,28 @@ class gen_hft_1d(AbstractJob):
         self.system_syntax = {}
         real_time_str = "15:15:00"
         self.system_syntax[real_time_str] = "cd %s && source ./%s" % (crontab_dir, "local_hft_1d.yml")
+        self.time_list = self.system_syntax.keys()
+        self.check_run()
+
+
+
+
+
+class local_etf_log(AbstractJob):
+    """
+    下载日志，signal collect
+    """
+
+    def __init__(self, ):
+        super(local_etf_log, self).__init__()
+        self.job_name = 'local_etf_log'
+        self.system_syntax = {}
+        self.dependent_job_list = []
+        self.run_daytime = True
+        self.max_running_second = 3600
+        self.send_email = 1
+        self.system_syntax = {}
+        self.system_syntax["11:45:00"] = "cd %s && source ./%s.yml" % (crontab_dir, self.job_name)
+        self.system_syntax["15:15:00"] = "cd %s && source ./%s.yml" % (crontab_dir, self.job_name)
         self.time_list = self.system_syntax.keys()
         self.check_run()
